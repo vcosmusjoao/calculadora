97b6bcfe6c425cd9a1dafeb335df0c0255f9ddce
vcosmusjoao <rm88372@fiap.com.br>
2021-08-15 11:15:07 -0300

    funcção clean para limpar numeros digitados

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,4 +1,8 @@
 function insert(num){
-  document.getElementById('resultado').innerHTML=num;
+var numero=  document.getElementById('resultado').innerHTML;
+document.getElementById('resultado').innerHTML=numero+num;
 
+}
+function clean(){
+  document.getElementById('resultado').innerHTML=" ";
 }
